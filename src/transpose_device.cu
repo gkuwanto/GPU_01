cb42efa451a3fb99b051ebff6d80d829e29d66b2
Garry Kuwanto <gkuwanto@gmail.com>
2021-02-10 11:03:52 +0700

    change array size

--- src/transpose_device.cu
+++ src/transpose_device.cu
@@ -54,7 +54,7 @@ void shmemTransposeKernel(const float *input, float *output, int n) {
     // padding). Again, comment on all sub-optimal accesses.
 
     // __shared__ float data[???];
-    __shared__ float s_input[64*16]; // the number of thread's per block
+    __shared__ float s_input[64*64]; // the number of thread's per block
     
     const int i = threadIdx.x + 64 * blockIdx.x;
     const int j = threadIdx.y + 64 * blockIdx.y;
